e740aa728c235fb27fcb73b55a8b35dcb1842f66
imsamurai <imsamurai@imsamurai-2>
2014-08-26 16:29:50 +0300

    fix class load

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -8,30 +8,18 @@
  * 
  */
 
-if (!class_exists('FixtureInjector', false)) {
-	App::uses('FixtureInjector', 'Environment.TestSuite/Fixture');
-	App::load('FixtureInjector');
-}
-if (!class_exists('CakeTestRunner', false)) {
-	App::uses('CakeTestRunner', 'Environment.TestSuite');
-	App::load('CakeTestRunner');
-}
-if (!class_exists('HHVMTestSuiteDispatcher', false)) {
-	App::uses('HHVMTestSuiteDispatcher', 'Environment.TestSuite');
-	App::load('HHVMTestSuiteDispatcher');
-}
-if (!class_exists('CakeBaseReporter', false)) {
-	App::uses('CakeBaseReporter', 'Environment.TestSuite/Reporter');
-	App::load('CakeBaseReporter');
-}
-if (!class_exists('CakeHtmlReporter', false)) {
-	App::uses('CakeHtmlReporter', 'Environment.TestSuite/Reporter');
-	App::load('CakeHtmlReporter');
-}
-if (!class_exists('CakeTestSuiteCommand', false)) {
-	App::uses('CakeTestSuiteCommand', 'Environment.TestSuite');
-	App::load('CakeTestSuiteCommand');
-}
+App::uses('FixtureInjector', 'Environment.TestSuite/Fixture');
+App::load('FixtureInjector');
+App::uses('CakeTestRunner', 'Environment.TestSuite');
+App::load('CakeTestRunner');
+App::uses('CakeTestSuiteCommand', 'Environment.TestSuite');
+App::load('CakeTestSuiteCommand');
+App::uses('HHVMTestSuiteDispatcher', 'Environment.TestSuite');
+App::load('HHVMTestSuiteDispatcher');
+App::uses('CakeBaseReporter', 'Environment.TestSuite/Reporter');
+App::load('CakeBaseReporter');
+App::uses('CakeHtmlReporter', 'Environment.TestSuite/Reporter');
+App::load('CakeHtmlReporter');
 
 Configure::write('Environment', Hash::mergeDiff(array(
 			'console' => array(
