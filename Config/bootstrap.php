65264c8004d10a7d1b186b9f20b427745341f22e
imsamurai <imsamurai@imsamurai-2>
2014-08-26 16:19:50 +0300

    fix class load

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -7,27 +7,30 @@
  * Format: http://book.cakephp.org/2.0/en/views.html
  * 
  */
-App::uses('FixtureInjector', 'Environment.TestSuite/Fixture');
-App::uses('CakeBaseReporter', 'Environment.TestSuite/Reporter');
-App::uses('CakeHtmlReporter', 'Environment.TestSuite/Reporter');
-App::uses('CakeTestSuiteCommand', 'Environment.TestSuite');
-App::uses('CakeTestRunner', 'Environment.TestSuite');
-App::uses('HHVMTestSuiteDispatcher', 'Environment.TestSuite');
 
-if (!class_exists('FixtureInjector')) {
+if (!class_exists('FixtureInjector', false)) {
+	App::uses('FixtureInjector', 'Environment.TestSuite/Fixture');
 	App::load('FixtureInjector');
 }
-if (!class_exists('CakeTestRunner')) {
+if (!class_exists('CakeTestRunner', false)) {
+	App::uses('CakeTestRunner', 'Environment.TestSuite');
 	App::load('CakeTestRunner');
 }
-if (!class_exists('HHVMTestSuiteDispatcher')) {
+if (!class_exists('HHVMTestSuiteDispatcher', false)) {
+	App::uses('HHVMTestSuiteDispatcher', 'Environment.TestSuite');
 	App::load('HHVMTestSuiteDispatcher');
 }
-if (!class_exists('CakeBaseReporter')) {
+if (!class_exists('CakeBaseReporter', false)) {
+	App::uses('CakeBaseReporter', 'Environment.TestSuite/Reporter');
 	App::load('CakeBaseReporter');
 }
-if (!class_exists('CakeHtmlReporter')) {
+if (!class_exists('CakeHtmlReporter', false)) {
+	App::uses('CakeHtmlReporter', 'Environment.TestSuite/Reporter');
 	App::load('CakeHtmlReporter');
+}
+if (!class_exists('CakeTestSuiteCommand', false)) {
+	App::uses('CakeTestSuiteCommand', 'Environment.TestSuite');
+	App::load('CakeTestSuiteCommand');
 }
 
 Configure::write('Environment', Hash::mergeDiff(array(
